494a4d220117ae010d9ae17734dd1e0e4177efbe
3str <64540028+3strteam@users.noreply.github.com>
2022-01-08 16:17:32 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+// var phone = "<div class='phone'>\n<img src='set.png' alt='' class='set' onclick='show()'>\n<div class='content'>\n<div class='text title'>\nButton\n</div>\n<div class='text condition'>Condition: queue</div>\n<div class='text score'>Score: 9000</div>\n<div class='text place'>Place: 3</div>\n<div class='btn-field'>\n<div class='btn'></div>\n</div>\n</div>\n</div>";
+// var no_phone = "<img src='set.png' alt='' class='set' onclick='show()'>\n<div class='content'>\n<div class='text title'>\nButton\n</div>\n<div class='text condition'>Condition: queue</div>\n<div class='text score'>Score: 9000</div>\n<div class='text place'>Place: 3</div>\n<div class='btn-field'>\n<div class='btn'></div>\n</div>\n</div>";
+// var field = document.querySelector("body .phone");
+
+// var content = document.querySelector("body").innerHTML;
+// if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+// 	if(content != no_phone) {
+// 		document.querySelector("body").innerHTML = no_phone;
+// 	}
+// 	field = document.querySelector("body");
+// } else {
+// 	if(content != phone) {
+// 		document.querySelector("body").innerHTML = phone;
+// 	}
+// 	field = document.querySelector("body .phone");
+// }
